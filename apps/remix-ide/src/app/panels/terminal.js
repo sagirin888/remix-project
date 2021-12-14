00fa004e7004f8dd6e66152d0cf06c12446b3346
David Disu <daviddisu8@gmail.com>
2021-12-14 16:57:55 +0100

    create instance setup

--- apps/remix-ide/src/app/panels/terminal.js
+++ apps/remix-ide/src/app/panels/terminal.js
@@ -22,7 +22,7 @@ function register (api) { KONSOLES.push(api) }
 const profile = {
   displayName: 'Terminal',
   name: 'terminal',
-  methods: ['log'],
+  methods: ['log', 'logHtml'],
   events: [],
   description: ' - ',
   version: packageJson.version
